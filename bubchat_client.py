695eb8e949dad144ae57735bc935cd36ce1cea97
noahmedoff@gmail.com <noahmedoff@gmail.com>
2024-05-22 12:54:43 -0700

    setup pycord code and run bot successful

--- bubchat_client.py
+++ bubchat_client.py
@@ -1,6 +1,14 @@
+import discord
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 bot_token = os.environ.get("BOT_KEY")
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+    
+bot.run(bot_token)
